b23d4da1ed0bf4812d64ab318b8d01b0f1b63243
linkedblogs <github@linkedblogs.org>
2018-04-15 17:29:24 +0400

    patterns removed

--- planet/urls.py
+++ planet/urls.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-try:
-    # Django 1.6
-    from django.conf.urls import patterns, url
-except ImportError:
-    # Django < 1.6
-    from django.conf.urls.defaults import patterns, url
+from django.conf.urls import url, include
+
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.sitemaps import views as sitemaps_views
@@ -26,7 +22,7 @@ else:
     url_add_feed_tuple = url(r'^feeds/add/$', FeedAddView.as_view(form_class=FeedAddForm), name="planet_feed_add")
 
 
-urlpatterns = patterns('planet.views',
+urlpatterns = [
     url(r'^blogs/(?P<pk>\d+)/delete/$', login_required(BlogDeleteView.as_view()), name="planet_blog_delete"),
     url(r'^blogs/(?P<blog_id>\d+)/(?P<slug>[a-zA-Z0-9_\-]+)/$', "blog_detail", name="planet_blog_detail"),
     url(r'^blogs/(?P<blog_id>\d+)/$', "blog_detail"),
@@ -58,23 +54,23 @@ urlpatterns = patterns('planet.views',
     url(r'^search/$', "search", name="planet_search"),
 
     url(r'^$', "index", name="planet_index"),
-)
+]
 
 # Feed's urls
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^posts/feeds/rss/$', PostFeed(), name="planet_rss_feed"),
     url(r'^feeds/rss/tags/(?P<tag>.*)/$', TagFeed(), name="planet_tag_rss_feed"),
     url(r'^feeds/rss/authors/(?P<author_id>\d+)/$', AuthorFeed(), name="planet_author_rss_feed"),
     url(r'^feeds/rss/authors/(?P<author_id>\d+)/tags/(?P<tag>.*)/$', AuthorTagFeed(), name="planet_tag_author_rss_feed"),
-)
+]
 
 # sitemaps
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^sitemap.xml$',
         cache_page(86400)(sitemaps_views.index),
         {'sitemaps': planet_sitemaps_dict, 'sitemap_url_name': 'sitemaps'}),
     url(r'^sitemap-(?P<section>.+)\.xml$',
         cache_page(86400)(sitemaps_views.sitemap),
         {'sitemaps': planet_sitemaps_dict}, name='sitemaps'),
-)
+]
 
